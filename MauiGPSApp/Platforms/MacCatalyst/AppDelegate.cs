6f5732fed7030adc7f835db32a3eb5ce35cf5a9f
baitong0402 <sukanya@generwiz.com>
2024-12-20 14:13:01 +0700

    create app

---
+++ MauiGPSApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiGPSApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
